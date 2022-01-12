84f2f30aff1b58f1c6fc3344cb2487132f879d3c
gabrielpatricksouza <gabrielpatricksouza777@gmail.com>
2022-01-12 00:15:49 -0300

    commit 35: - atualizações

--- lib/app/modules/home/view/home_page.dart
+++ lib/app/modules/home/view/home_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +15,8 @@ import 'package:uni_match/constants/constants.dart';
 import 'package:uni_match/dialogs/vip_dialog.dart';
 import 'package:uni_match/widgets/notification_counter.dart';
 import 'package:uni_match/widgets/svg_icon.dart';
-
+import 'package:update_app/bean/download_process.dart';
+import 'package:update_app/update_app.dart';
 import 'dragable_button.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -93,6 +95,7 @@ class _HomeScreenState extends ModularState<HomeScreen, HomeStore> {
                          icon: SvgIcon("assets/icons/beer_icon.svg",
                            width: 30, height: 30, color: Theme.of(context).iconTheme.color,),
                          onPressed: (){
+                           // Navigator.push(context, MaterialPageRoute(builder: (_) => MyApp()));
                            Modular.to.pushNamed('/party');
                          }),
 
@@ -230,5 +233,125 @@ class _HomeScreenState extends ModularState<HomeScreen, HomeStore> {
             return NotificationCounter(icon: icon, counter: total);
           }
         });
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //定时更新进度
+  Timer? timer;
+
+  //下载进度
+  double downloadProcess = 0;
+
+  //下载状态
+  String downloadStatus = "";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext? context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Update app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(16),
+                width: 200,
+                height: 200,
+                child: CircularProgressIndicator(
+                  value: downloadProcess,
+                  strokeWidth: 10,
+                ),
+              ),
+              Text("Download status: $downloadStatus"),
+              Text("Please replace the download url with your own address"),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'click right bottom download button download!',
+                  style: TextStyle(color: Theme.of(context!).primaryColor),
+                ),
+                Text('Adapt to Android 6.0'),
+                Text('Adapt to Android 7.0'),
+                Text('Adapt to Android 8.0'),
+                Text('Adapt to Android 9.0'),
+                Text('Adapt to Android 10.0'),
+                Text('Adapt to Android 10.0'),
+                Text('Add download progress monitoring'),
+              ],
+            )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: download,
+          child: Icon(Icons.file_download),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  void download() async {
+    var downloadId = await UpdateApp.updateApp(
+        url:
+        "https://vinicula.gabrielpatricksouza.com/app-release.apk",
+        appleId: "");
+
+    //本地已有一样的apk, 下载成功
+    if (downloadId == 0) {
+      setState(() {
+        downloadProcess = 1;
+        downloadStatus = ProcessState.STATUS_SUCCESSFUL.toString();
+      });
+      return;
+    }
+
+    //出现了错误, 下载失败
+    if (downloadId == -1) {
+      setState(() {
+        downloadProcess = 1;
+        downloadStatus = ProcessState.STATUS_FAILED.toString();
+      });
+      return;
+    }
+
+    //正在下载文件
+    timer = Timer.periodic(Duration(milliseconds: 100), (timer) async {
+      var process = await UpdateApp.downloadProcess(downloadId: downloadId);
+      //更新界面状态
+      setState(() {
+        downloadProcess = process.current / process.count;
+        downloadStatus = process.status.toString();
+      });
+
+      if (process.status == ProcessState.STATUS_SUCCESSFUL ||
+          process.status == ProcessState.STATUS_FAILED) {
+        //如果已经下载成功, 取消计时
+        timer.cancel();
+      }
+    });
   }
 }
